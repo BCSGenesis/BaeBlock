30b0d124b9996d243271a78d6a2f6c20892df985
Prkskhn <tjrgns7452@gmail.com>
2023-07-04 10:42:41 +0900

    building page

---
+++ src/pages/rider_market.jsx
@@ -0,0 +1,5 @@
+const RiderMarket = () => {
+  return <div>라이더 nft 마켓</div>;
+};
+
+export default RiderMarket;
---
+++ src/pages/store_market.jsx
@@ -0,0 +1,5 @@
+const StoreMarket = () => {
+  return <div> 스토어 nft 마켓</div>;
+};
+
+export default StoreMarket;
